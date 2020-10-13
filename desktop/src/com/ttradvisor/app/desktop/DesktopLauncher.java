f49d3836551e4296e2f4cbe66f54508a9dc3040b
Andrey Vorontsov <avorontsov@wisc.edu>
2020-10-13 18:17:37 -0500

    LibGDX setup

---
+++ desktop/src/com/ttradvisor/app/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ttradvisor.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ttradvisor.app.TTRAdvisorApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TTRAdvisorApp(), config);
+	}
+}
