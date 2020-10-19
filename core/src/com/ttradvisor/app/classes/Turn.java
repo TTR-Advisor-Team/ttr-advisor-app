c3fe04d41d5ee10f28b44842308bb146a739c8c5
sthomason1 <36902541+sthomason1@users.noreply.github.com>
2020-10-19 17:54:40 -0500

    ACTUALLY added constructor and getters/setters to turn (wasn't included in last commit)

--- core/src/com/ttradvisor/app/classes/Turn.java
+++ core/src/com/ttradvisor/app/classes/Turn.java
@@ -1,5 +1,41 @@
 package com.ttradvisor.app.classes;
 
+import java.util.ArrayList;
+
 public class Turn {
+	private Board snapshot;
+	private Action action;
+	private ArrayList<Player> playerSnapshots;
+	
+	public Turn(Board snapshot, Action action, ArrayList<Player> playerSnapshots) {
+		this.setSnapshot(snapshot);
+		this.setAction(action);
+		this.setPlayerSnapshots(playerSnapshots);
+	}
+
+	public Board getSnapshot() {
+		return snapshot;
+	}
+
+	public void setSnapshot(Board snapshot) {
+		this.snapshot = snapshot;
+	}
+
+	public Action getAction() {
+		return action;
+	}
+
+	public void setAction(Action action) {
+		this.action = action;
+	}
+
+	public ArrayList<Player> getPlayerSnapshots() {
+		return playerSnapshots;
+	}
+
+	public void setPlayerSnapshots(ArrayList<Player> playerSnapshots) {
+		this.playerSnapshots = playerSnapshots;
+	}
+	
 
 }
