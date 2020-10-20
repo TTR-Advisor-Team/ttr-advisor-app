42c87843d09ebba2b91ca91d7146732cc7c90549
Andrey Vorontsov <avorontsov@wisc.edu>
2020-10-20 00:50:51 -0500

    crummy test of camera movement

--- core/src/com/ttradvisor/app/screens/GameScreen.java
+++ core/src/com/ttradvisor/app/screens/GameScreen.java
@@ -1,12 +1,17 @@
 package com.ttradvisor.app.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.ttradvisor.app.TTRAdvisorApp;
 
@@ -19,65 +24,97 @@ import java.time.Instant;
  */
 public class GameScreen implements Screen {
 
-    private Stage stage;
     private TTRAdvisorApp mainApp;
+    private InputMultiplexer inputMult;
+    private Stage mapStage;
     private OrthographicCamera camera;
+    private Stage guiStage;
 
-    private final int startX = 1100;// -Gdx.graphics.getWidth()/2;
-    private final int startY = 1225;
+//    private final int startX = 1100;// -Gdx.graphics.getWidth()/2;
+//    private final int startY = 1225;
+    
+    private float cameraX;
+    private float cameraY;
 
-    private final int endX = 2350;// -Gdx.graphics.getWidth()/2;
-    private final int endY = 600;
-
-    private float minAltitude = 0.5f;
-    private float maxAltitude = 2.5f;
-
-    private float percent;
-    private long startTime;
-    private final float animation_duration = 15000;
+//    private final int endX = 2350;// -Gdx.graphics.getWidth()/2;
+//    private final int endY = 600;
+//
+//    private float minAltitude = 0.5f;
+//    private float maxAltitude = 2.5f;
+//
+//    private float percent;
+//    private long startTime;
+//    private final float animation_duration = 15000;
 
     public GameScreen(TTRAdvisorApp main) {
         mainApp = main;
-        stage = new Stage(new ScreenViewport());
+        guiStage = new Stage(new ScreenViewport());
+        mapStage = new Stage(new ScreenViewport());
+        inputMult = new InputMultiplexer(guiStage, mapStage);
+        
+        setupMapInputHandling();
 
         Image map = new Image(new Texture("map.jpg"));
-        stage.addActor(map);
-        camera = (OrthographicCamera) stage.getViewport().getCamera();
-        camera.translate(startX,startY);
-        startTime = Instant.now().toEpochMilli();
+        
+        mapStage.addActor(map);
+        camera = (OrthographicCamera) mapStage.getViewport().getCamera();
+        camera.setToOrtho(true);
+        // startTime = Instant.now().toEpochMilli();
 
+    }
+    
+    
+    // Andrey's thoughts
+    // might need to do these events on the map Image instead
+    // stage vs. world coordinates (?)
+    private void setupMapInputHandling() {
+    	mapStage.addListener(new ClickListener() {
+
+    		@Override
+    		public void clicked(InputEvent event, float x, float y) {
+    			cameraX = x;
+    			cameraY = y;
+    			camera.position.set(cameraX, cameraY, camera.position.z);
+    			super.clicked(event, x, y);
+    		}
+    	});
     }
 
     @Override
     public void show() {
         Gdx.app.log("MainScreen","show");
-        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(inputMult);
 
     }
 
     @Override
     public void render(float delta) {
-        //jave 8
-        long secondFromStart = Instant.now().toEpochMilli()-startTime;
-        percent = (secondFromStart%animation_duration)/animation_duration;
-        percent = (float)Math.cos(percent*Math.PI*2)/2+0.5f;
-        Gdx.app.log("render","secondFromStart:"+ secondFromStart+", %:"+percent);
+//        //java 8
+//        long secondFromStart = Instant.now().toEpochMilli()-startTime;
+//        percent = (secondFromStart%animation_duration)/animation_duration;
+//        percent = (float)Math.cos(percent*Math.PI*2)/2+0.5f;
+//        Gdx.app.log("render","secondFromStart:"+ secondFromStart+", %:"+percent);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         moveCamera();
-        stage.act();
-        stage.draw();
+        
+        mapStage.act();
+        guiStage.act();
+        mapStage.draw();
+        guiStage.draw();
     }
 
     private void moveCamera(){
-        float currentX = startX + (endX-startX)*percent;
-        float currentY = startY + (endY-startY)*percent;
-        float percentZ = Math.abs(percent - 0.5f)*2;
-        float currentZ = maxAltitude - (maxAltitude-minAltitude)*percentZ  ;
-
-        camera.position.x = currentX;
-        camera.position.y = currentY;
-        camera.zoom = currentZ;
+//        float currentX = startX + (endX-startX)*percent;
+//        float currentY = startY + (endY-startY)*percent;
+//        float percentZ = Math.abs(percent - 0.5f)*2;
+//        float currentZ = maxAltitude - (maxAltitude-minAltitude)*percentZ  ;
+//
+//        camera.position.x = currentX;
+//        camera.position.y = currentY;
+//        camera.zoom = currentZ;
+    	
         camera.update();
     }
 
@@ -104,7 +141,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        stage.dispose();
+        mapStage.dispose();
+        guiStage.dispose();
     }
 
 
