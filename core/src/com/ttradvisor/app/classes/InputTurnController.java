528a21776a7fa2dc8c6d6ad1b1d8875a944959b8
Andrey Vorontsov <avorontsov@wisc.edu>
2020-10-20 00:25:25 -0500

    turn controller - interface only

---
+++ core/src/com/ttradvisor/app/classes/InputTurnController.java
@@ -0,0 +1,25 @@
+package com.ttradvisor.app.classes;
+
+/**
+ *	Contains logic and validation for inputting new turns. 
+ *	Determines whether the new turn created is valid and updates the View
+ */
+public class InputTurnController {
+
+	/**
+	 * Manage and validate the input of the initial turn
+	 * (drawing 4 Train Cards and 2-3 Destination Tickets)
+	 */
+	public void initialTurn() {
+		
+	}
+	
+	/**
+	 * Process an Action object as the next turn.
+	 * @param thisTurn the Action taken for the current player's turn
+	 */
+	public void takeAction(Action thisTurn) {
+		
+	}
+
+}
