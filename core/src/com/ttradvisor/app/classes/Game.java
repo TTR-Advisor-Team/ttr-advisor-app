fd1385fa393a088bccb41b37c13fa861aa30b5ca
mwolak5 <47951017+mwolak5@users.noreply.github.com>
2020-10-19 20:27:45 -0500

    Create Game.java

---
+++ core/src/com/ttradvisor/app/classes/Game.java
@@ -0,0 +1,44 @@
+package com.ttradvisor.app.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+	private ArrayList<Player> curPlayers;
+	private Board curBoard;
+	//private Recommender rec;
+	private ArrayList<Turn> turns;
+	
+	public Game(ArrayList<Player> players, Board board, ArrayList<Turn> turns) {
+		curPlayers = players;
+		curBoard = board;
+		this.turns = turns;
+	}
+	
+	public void addTurn(Turn newTurn) {
+		turns.add(newTurn);
+	}
+	
+	public List<Player> getPlayers() {
+		return curPlayers;
+	}
+	
+	public void setPlayers(ArrayList<Player> players) {
+		curPlayers = players;
+	}
+	
+	public Board getBoard() {
+		return curBoard;
+	}
+	
+	public void setBoard(Board board) {
+		curBoard = board;
+	}
+	public List<Turn> getTurns(){
+		return turns;
+	}
+	public void setTurns(ArrayList<Turn> turns) {
+		this.turns = turns;
+	}
+}
