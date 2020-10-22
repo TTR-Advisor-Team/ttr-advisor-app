3042bf84d6d12f66d9d5f07ff8cc3919106ae755
Andrey Vorontsov <avorontsov@wisc.edu>
2020-10-21 22:56:24 -0500

    Zoom/pan camera controls

--- core/src/com/ttradvisor/app/screens/GameScreen.java
+++ core/src/com/ttradvisor/app/screens/GameScreen.java
@@ -6,12 +6,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.ttradvisor.app.TTRAdvisorApp;
 
@@ -29,15 +29,10 @@ public class GameScreen implements Screen {
     private Stage mapStage;
     private OrthographicCamera camera;
     private Stage guiStage;
-
-//    private final int startX = 1100;// -Gdx.graphics.getWidth()/2;
-//    private final int startY = 1225;
     
-    private float cameraX;
-    private float cameraY;
+    private float mapWidth;
+    private float mapHeight;
 
-//    private final int endX = 2350;// -Gdx.graphics.getWidth()/2;
-//    private final int endY = 600;
 //
 //    private float minAltitude = 0.5f;
 //    private float maxAltitude = 2.5f;
@@ -54,30 +49,97 @@ public class GameScreen implements Screen {
         
         setupMapInputHandling();
 
-        Image map = new Image(new Texture("map.jpg"));
+        Image map = new Image(new Texture("low_res_board.jpg"));
+        
+        // notes: doing this is risky (I think) because texture coordinates != world coordinates
+        // but I guess it's fine as long as we never scale the textures independently of the world
+        // (so that 1px in texture is always == 1px in world)
+        mapWidth = map.getWidth();
+        mapHeight = map.getHeight();
         
         mapStage.addActor(map);
         camera = (OrthographicCamera) mapStage.getViewport().getCamera();
-        camera.setToOrtho(true);
+        camera.setToOrtho(false);
+        camera.zoom = 1;
+        
+        camera.position.set(0, 0, camera.position.z);
         // startTime = Instant.now().toEpochMilli();
 
     }
     
-    
-    // Andrey's thoughts
-    // might need to do these events on the map Image instead
-    // stage vs. world coordinates (?)
+    /**
+     * Handlers for mapStage
+     * ActorGestureListener used here for sake of identifying zooms/pans on android
+     */
     private void setupMapInputHandling() {
-    	mapStage.addListener(new ClickListener() {
-
+    	
+    	mapStage.addListener(new InputListener() {
+    		
     		@Override
-    		public void clicked(InputEvent event, float x, float y) {
-    			cameraX = x;
-    			cameraY = y;
-    			camera.position.set(cameraX, cameraY, camera.position.z);
-    			super.clicked(event, x, y);
+    		public boolean scrolled(InputEvent event, float x, float y, int amount) {
+    			camera.zoom += (float) amount / 10f;
+    		    Gdx.app.log("Camera", "Zoomed to: " + camera.zoom);
+    			return super.scrolled(event, x, y, amount);
+    		}
+    		
+    	});
+
+    	mapStage.addListener(new ActorGestureListener() {
+    		
+    		// screen (not world) coordinates of the start of the pan and current pan
+    		private Vector3 panOriginScreen;
+    		private Vector3 panCurrentScreen;
+    		private Vector3 trueScreenDelta;
+    		// world coordinates of camera when starting the pan
+    		private Vector3 panOriginCamera;
+    		
+    		@Override
+    		public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
+    			
+    			// WORK IN PROGRESS (works but maybe not for all situations)
+    			
+    			if (panOriginScreen == null) {
+    				panOriginScreen = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+    				panOriginCamera = new Vector3(camera.position);
+    			}
+    			panCurrentScreen = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+    			
+    			trueScreenDelta = new Vector3(panOriginScreen.x - panCurrentScreen.x, panOriginScreen.y - panCurrentScreen.y, 0);
+    		
+    			camera.position.set(panOriginCamera.x + trueScreenDelta.x, panOriginCamera.y - trueScreenDelta.y, camera.position.z);
+    		    Gdx.app.log("Camera", "Translated to coords: " + camera.position.x + ", " + camera.position.y);
+    			super.pan(event, x, y, deltaX, deltaY);
+    		}
+    		
+    		@Override
+    		public void panStop(InputEvent event, float x, float y, int pointer, int button) {
+    			if (camera.position.x < 0) {
+    				camera.position.x = 0;
+    			}
+    			else if (camera.position.x > mapWidth) {
+    				camera.position.x = mapWidth;
+    			}
+    			if (camera.position.y < 0) {
+    				camera.position.y = 0;
+    			}
+    			else if (camera.position.y > mapHeight) {
+    				camera.position.y = mapHeight;
+    			}
+    			camera.position.set(camera.position.x, camera.position.y, camera.position.z);
+    		    Gdx.app.log("Camera", "Snapped to coords: " + camera.position.x + ", " + camera.position.y);
+    		    panOriginScreen = null;
+    			super.panStop(event, x, y, pointer, button);
+    		}
+    		
+    		@Override
+    		public void zoom(InputEvent event, float initialDistance, float distance) {
+    		    float ratio = (initialDistance / distance) * camera.zoom;
+    		    camera.zoom = ratio;
+    		    Gdx.app.log("Camera", "Zoomed to: " + camera.zoom);
+    			super.zoom(event, initialDistance, distance);
     		}
     	});
+
     }
 
     @Override
