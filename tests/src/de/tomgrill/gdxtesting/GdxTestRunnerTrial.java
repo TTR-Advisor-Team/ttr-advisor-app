50b8a91943603ad0921bbd289c350242e504e73b
Andrey Vorontsov <avorontsov@wisc.edu>
2020-11-12 19:04:35 -0600

    Comment the unused stuff out

--- tests/src/de/tomgrill/gdxtesting/GdxTestRunnerTrial.java
+++ tests/src/de/tomgrill/gdxtesting/GdxTestRunnerTrial.java
@@ -1,114 +1,114 @@
-// Andrey's note - trial at making the JUnit testing work
-
-package de.tomgrill.gdxtesting;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.runner.notification.RunNotifier;
-import org.junit.runners.BlockJUnit4ClassRunner;
-import org.junit.runners.model.FrameworkMethod;
-import org.junit.runners.model.InitializationError;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.ttradvisor.app.TTRAdvisorApp;
-
-import static org.mockito.Mockito.mock;
-
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
-/**
- * Sourced from https://github.com/TomGrill/gdx-testing
- *
- */
-public class GdxTestRunnerTrial extends BlockJUnit4ClassRunner implements ApplicationListener {
-	
-	public static TTRAdvisorApp testApp;
-
-	protected Map<FrameworkMethod, RunNotifier> invokeInRender = new HashMap<FrameworkMethod, RunNotifier>();
-	
-	public GdxTestRunnerTrial(Class<?> klass) throws InitializationError {
-		super(klass);
-		
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		
-		config.pauseWhenMinimized = true;
-		
-		// config options set for testing
-		
-		config.width = 1080;
-		config.height = 720;
-//		config.fullscreen = true;
-		config.resizable = false;
-		config.title = "Ticket to Ride Advisor";
-		
-		testApp = new TTRAdvisorAppTest(this);
-		new LwjglApplication(testApp, config);
-		
-
-		// guiStage.getRoot().findActor(name);
-	}
-
-	@Override
-	public void create() {
-	}
-
-	@Override
-	public void resume() {
-	}
-
-	@Override
-	public void render() {
-		synchronized (invokeInRender) {
-			for (Map.Entry<FrameworkMethod, RunNotifier> each : invokeInRender.entrySet()) {
-				super.runChild(each.getKey(), each.getValue());
-			}
-			invokeInRender.clear();
-		}
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void dispose() {
-	}
-
-	@Override
-	protected void runChild(FrameworkMethod method, RunNotifier notifier) {
-		synchronized (invokeInRender) {
-			// add for invoking in render phase, where gl context is available
-			invokeInRender.put(method, notifier);
-		}
-		// wait until that test was invoked
-		waitUntilInvokedInRenderMethod();
-	}
-
-	/**
-	    *
-	    */
-	protected void waitUntilInvokedInRenderMethod() {
-		try {
-			while (true) {
-				Thread.sleep(10);
-				synchronized (invokeInRender) {
-					if (invokeInRender.isEmpty())
-						break;
-				}
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-}
+//// Andrey's note - trial at making the JUnit testing work
+//
+//package de.tomgrill.gdxtesting;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.junit.runner.notification.RunNotifier;
+//import org.junit.runners.BlockJUnit4ClassRunner;
+//import org.junit.runners.model.FrameworkMethod;
+//import org.junit.runners.model.InitializationError;
+//
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.graphics.GL20;
+//import com.ttradvisor.app.TTRAdvisorApp;
+//
+//import static org.mockito.Mockito.mock;
+//
+//import com.badlogic.gdx.ApplicationListener;
+//import com.badlogic.gdx.backends.headless.HeadlessApplication;
+//import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+//import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+//import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+//
+///**
+// * Sourced from https://github.com/TomGrill/gdx-testing
+// *
+// */
+//public class GdxTestRunnerTrial extends BlockJUnit4ClassRunner implements ApplicationListener {
+//	
+//	public static TTRAdvisorApp testApp;
+//
+//	protected Map<FrameworkMethod, RunNotifier> invokeInRender = new HashMap<FrameworkMethod, RunNotifier>();
+//	
+//	public GdxTestRunnerTrial(Class<?> klass) throws InitializationError {
+//		super(klass);
+//		
+//		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//		
+//		config.pauseWhenMinimized = true;
+//		
+//		// config options set for testing
+//		
+//		config.width = 1080;
+//		config.height = 720;
+////		config.fullscreen = true;
+//		config.resizable = false;
+//		config.title = "Ticket to Ride Advisor";
+//		
+//		testApp = new TTRAdvisorAppTest(this);
+//		new LwjglApplication(testApp, config);
+//		
+//
+//		// guiStage.getRoot().findActor(name);
+//	}
+//
+//	@Override
+//	public void create() {
+//	}
+//
+//	@Override
+//	public void resume() {
+//	}
+//
+//	@Override
+//	public void render() {
+//		synchronized (invokeInRender) {
+//			for (Map.Entry<FrameworkMethod, RunNotifier> each : invokeInRender.entrySet()) {
+//				super.runChild(each.getKey(), each.getValue());
+//			}
+//			invokeInRender.clear();
+//		}
+//	}
+//
+//	@Override
+//	public void resize(int width, int height) {
+//	}
+//
+//	@Override
+//	public void pause() {
+//	}
+//
+//	@Override
+//	public void dispose() {
+//	}
+//
+//	@Override
+//	protected void runChild(FrameworkMethod method, RunNotifier notifier) {
+//		synchronized (invokeInRender) {
+//			// add for invoking in render phase, where gl context is available
+//			invokeInRender.put(method, notifier);
+//		}
+//		// wait until that test was invoked
+//		waitUntilInvokedInRenderMethod();
+//	}
+//
+//	/**
+//	    *
+//	    */
+//	protected void waitUntilInvokedInRenderMethod() {
+//		try {
+//			while (true) {
+//				Thread.sleep(10);
+//				synchronized (invokeInRender) {
+//					if (invokeInRender.isEmpty())
+//						break;
+//				}
+//			}
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//}
--- tests/src/de/tomgrill/gdxtesting/TTRAdvisorAppTest.java
+++ tests/src/de/tomgrill/gdxtesting/TTRAdvisorAppTest.java
@@ -1,79 +1,79 @@
-// Andrey's note - another attempt at running JUnit with the OpenGL context active for our app.
-// It did not work.
-
-package de.tomgrill.gdxtesting;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.runner.notification.RunNotifier;
-import org.junit.runners.model.FrameworkMethod;
-
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.ttradvisor.app.TTRAdvisorApp;
-import com.ttradvisor.app.classes.Board;
-import com.ttradvisor.app.classes.Colors;
-import com.ttradvisor.app.classes.DestinationTicketList;
-import com.ttradvisor.app.classes.GameState;
-import com.ttradvisor.app.classes.HistoryController;
-import com.ttradvisor.app.classes.InputTurnController;
-import com.ttradvisor.app.classes.Player;
-import com.ttradvisor.app.classes.Turn;
-import com.ttradvisor.app.screens.TitleScreen;
-
-/**
- * Copied in as example code to modify
- *
- */
-public class TTRAdvisorAppTest extends TTRAdvisorApp {
-	
-	// FOR TESTING
-	private GdxTestRunnerTrial testRunner;
-	
-	static public Skin skin;
-	public Colors.player userColor;
-	
-	public InputTurnController turnInput;
-	public HistoryController hist;
-	public GameState gameState;
-	
-	public TTRAdvisorAppTest(GdxTestRunnerTrial testRunner) {
-		this.testRunner = testRunner;
-	}
-
-	@Override
-	public void create () {
-		gameState = new GameState(new ArrayList<Player>(), new Board("cities.txt"),
-				new DestinationTicketList("destinations.txt"), new ArrayList<Turn>());
-		turnInput = new InputTurnController(gameState);
-		hist = new HistoryController(gameState);
-		skin = new Skin(Gdx.files.internal("ui skin/glassy-ui.json"));
-		this.setScreen(new TitleScreen(this));
-	}
-
-	@Override
-	public void render () {
-		// FOR TESTING
-		synchronized (testRunner.invokeInRender) {
-			for (Map.Entry<FrameworkMethod, RunNotifier> each : testRunner.invokeInRender.entrySet()) {
-				testRunner.runChild(each.getKey(), each.getValue());
-			}
-			testRunner.invokeInRender.clear();
-		}
-		
-		super.render();
-	}
-	
-	@Override
-	public void dispose () {
-		skin.dispose();
-	}
-
-}
+//// Andrey's note - another attempt at running JUnit with the OpenGL context active for our app.
+//// It did not work.
+//
+//package de.tomgrill.gdxtesting;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.junit.runner.notification.RunNotifier;
+//import org.junit.runners.model.FrameworkMethod;
+//
+//import com.badlogic.gdx.ApplicationAdapter;
+//import com.badlogic.gdx.Game;
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.graphics.GL20;
+//import com.badlogic.gdx.graphics.Texture;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+//import com.ttradvisor.app.TTRAdvisorApp;
+//import com.ttradvisor.app.classes.Board;
+//import com.ttradvisor.app.classes.Colors;
+//import com.ttradvisor.app.classes.DestinationTicketList;
+//import com.ttradvisor.app.classes.GameState;
+//import com.ttradvisor.app.classes.HistoryController;
+//import com.ttradvisor.app.classes.InputTurnController;
+//import com.ttradvisor.app.classes.Player;
+//import com.ttradvisor.app.classes.Turn;
+//import com.ttradvisor.app.screens.TitleScreen;
+//
+///**
+// * Copied in as example code to modify
+// *
+// */
+//public class TTRAdvisorAppTest extends TTRAdvisorApp {
+//	
+//	// FOR TESTING
+//	private GdxTestRunnerTrial testRunner;
+//	
+//	static public Skin skin;
+//	public Colors.player userColor;
+//	
+//	public InputTurnController turnInput;
+//	public HistoryController hist;
+//	public GameState gameState;
+//	
+//	public TTRAdvisorAppTest(GdxTestRunnerTrial testRunner) {
+//		this.testRunner = testRunner;
+//	}
+//
+//	@Override
+//	public void create () {
+//		gameState = new GameState(new ArrayList<Player>(), new Board("cities.txt"),
+//				new DestinationTicketList("destinations.txt"), new ArrayList<Turn>());
+//		turnInput = new InputTurnController(gameState);
+//		hist = new HistoryController(gameState);
+//		skin = new Skin(Gdx.files.internal("ui skin/glassy-ui.json"));
+//		this.setScreen(new TitleScreen(this));
+//	}
+//
+//	@Override
+//	public void render () {
+//		// FOR TESTING
+//		synchronized (testRunner.invokeInRender) {
+//			for (Map.Entry<FrameworkMethod, RunNotifier> each : testRunner.invokeInRender.entrySet()) {
+//				testRunner.runChild(each.getKey(), each.getValue());
+//			}
+//			testRunner.invokeInRender.clear();
+//		}
+//		
+//		super.render();
+//	}
+//	
+//	@Override
+//	public void dispose () {
+//		skin.dispose();
+//	}
+//
+//}
